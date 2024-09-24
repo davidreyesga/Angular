856fbb074e7328450243984bbc0e9c22b7995f36
David Reyes <jtyuar@gmail.com>
2024-09-24 15:48:58 -0500

    session storage

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,5 +1,7 @@
+// Importa Router desde '@angular/router'
 import { Component, OnInit } from '@angular/core';
 import { RickAndMortyService } from '../rick-and-morty.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-characters',
@@ -9,11 +11,20 @@ import { RickAndMortyService } from '../rick-and-morty.service';
 export class CharactersComponent implements OnInit {
     characters: any[] = [];
 
-    constructor(private rickAndMortyService: RickAndMortyService) {}
+    constructor(private rickAndMortyService: RickAndMortyService, private router: Router) {}
 
     ngOnInit(): void {
         this.rickAndMortyService.getCharacters().subscribe(response => {
         this.characters = response.results;
         });
+    }
+
+    redirectToLogin(character: any): void {
+        // Guarda el nombre y el planeta del personaje en localStorage
+        sessionStorage.setItem('user', character.name);
+        sessionStorage.setItem('password', character.origin.name);
+
+        // Redirige al usuario al login
+        this.router.navigate(['/login']);
     }
 }
